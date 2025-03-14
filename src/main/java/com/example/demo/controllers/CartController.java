06ffdbe606d97a825af856334c41273334ea8c0f
Nicolas Peña <nicolaspj999@gmail.com>
2025-03-14 15:25:07 -0500

    primer commit

---
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -0,0 +1,70 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Movie;
+import com.example.demo.repositories.MovieRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping
+    public String index(HttpSession session, Model model) {
+        
+        List<Movie> movies = movieRepository.findAll();
+        
+        // Obtener productos del carrito almacenados en la sesión
+        Map<Long, Integer> cartMovieData = (Map<Long, Integer>) session.getAttribute("cart_movie_data");
+        Map<Long, Movie> cartMovies = new HashMap<>();
+
+        if (cartMovieData != null) {
+            for (Long id : cartMovieData.keySet()) {
+                Optional<Movie> movie = movieRepository.findById(id);
+                movie.ifPresent(p -> cartMovies.put(id, p));
+            }
+        }
+
+        model.addAttribute("title", "Cart - Online Store");
+        model.addAttribute("subtitle", "Shopping Cart");
+        model.addAttribute("movies", movies);
+        model.addAttribute("cartMovies", cartMovies);
+        return "cart/index";
+    }
+
+    @PostMapping("/add/{id}")
+    public String add(@PathVariable Long id, HttpSession session) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        if (movie.isEmpty()) {
+            return "redirect:/cart";
+        }
+
+        // Recuperar el carrito de la sesión o crear uno nuevo si es nulo
+        Map<Long, Integer> cartMovieData = (Map<Long, Integer>) session.getAttribute("cart_movie_data");
+        if (cartMovieData == null) {
+            cartMovieData = new HashMap<>();
+        }
+
+        // Agregar producto al carrito (contador de cantidad)
+        cartMovieData.put(id, cartMovieData.getOrDefault(id, 0) + 1);
+        session.setAttribute("cart_movie_data", cartMovieData);
+        return "redirect:/cart";
+    }
+
+    @GetMapping("/removeAll")
+    public String removeAll(HttpSession session) {
+        session.removeAttribute("cart_movie_data");
+        return "redirect:/cart";
+    }
+}
---
+++ src/main/java/com/example/demo/controllers/MovieController.java
@@ -0,0 +1,69 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Movie;
+import com.example.demo.repositories.MovieRepository; 
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@Controller
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping("/movies")
+    public String index(Model model) {
+        List<Movie> movies = movieRepository.findAll(); 
+        model.addAttribute("title", "Movies - MovieNest"); 
+        model.addAttribute("subtitle", "List of movies"); 
+        model.addAttribute("movies", movies);
+        return "movie/index"; 
+    }
+
+    @GetMapping("/movies/{id}") 
+    public String show(@PathVariable("id") Long id, Model model) { 
+        Movie movie = movieRepository.findById(id) .orElseThrow(() -> new RuntimeException("Movie not found")); 
+        model.addAttribute("title", movie.getName() + " - Online Store"); 
+        model.addAttribute("subtitle", movie.getName() + " - Movie information"); 
+        model.addAttribute("movie", movie);
+        return "movie/show";    
+    }
+    
+    @GetMapping("/movies/create")
+    public String createMovieForm(Model model) { 
+        model.addAttribute("movie", new Movie()); 
+        return "movie/create"; 
+    }
+
+    @GetMapping("/movies/success")
+    public String successPage() {
+        return "movie/success";
+    }
+
+    @PostMapping("/movies") 
+    public String save(Movie movie) {  
+        if (movie.getName() == null || movie.getName().isEmpty() || movie.getDescription() == null || movie.getDescription().isEmpty() || movie.getPrice() == null) { 
+            throw new RuntimeException("Name, Description and Price are required"); 
+        } 
+        movieRepository.save(movie); 
+        return "redirect:/movies/success"; 
+    }
+
+    @PostMapping("/movies/{id}/delete")
+    public String deleteMovie(@PathVariable("id") Long id) {
+        movieRepository.deleteById(id);
+        return "redirect:/movies";
+    }
+    
+}
